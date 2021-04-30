bc825313f450eab3d55491efa127b045e451a50a
Paul Neenan <paulmneenan@gmail.com>
2021-04-30 10:02:30 -0400

    db/orm/apolloserver set up

---
+++ src/mikro-orm.config.ts
@@ -0,0 +1,15 @@
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import { MikroORM } from "@mikro-orm/core"
+import path from 'path'
+
+export default {
+    migrations: {
+    path: path.join(__dirname, "./migrations"),
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+    },
+    entities: [Post],
+    dbName: "lireddit",
+    debug: !__prod__,
+    type: "postgresql"
+} as Parameters<typeof MikroORM.init>[0];
